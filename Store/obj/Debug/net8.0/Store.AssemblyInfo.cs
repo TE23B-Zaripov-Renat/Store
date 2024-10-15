1d589a0c88f8322313633761efe1d8be181fb654
Renat Zaripov <renat.zaripov@elev.ga.ntig.se>
2024-10-15 13:08:06 +0200

    ,kjhbljh

--- Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f744220be2d333e70538cc70a399be0f38900c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09eff71a533ef1122b406a5f32aa0e871b47dcf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
